9694a38f93466c1d23f651a72872c998b8c8ce8b
paustria <austrp1@unlv.nevada.edu>
2020-10-15 16:47:01 -0700

    added function to cat files from ipfs

--- client.js
+++ client.js
@@ -9,7 +9,7 @@ const PEER_ID = { name: 'test_peer_id' }
 
 main()
 
-async function main () {
+async function main() {
   /* creates IPFS instance */
   const ipfsNode = await ipfs.create()
   const version = await ipfsNode.version()
@@ -24,10 +24,25 @@ async function main () {
 
   const encryptResult = ibe.encrypt(
     ibeSetupResult.publicParameters, PEER_ID, fileContents)
-  console.log(JSON.stringify(encryptResult))
+  //console.log(JSON.stringify(encryptResult))
 
-  addFileToIPFS(ipfsNode, FILE_NAME, JSON.stringify(encryptResult))
+  const ipfsFile = await addFileToIPFS(
+    ipfsNode, FILE_NAME, JSON.stringify(encryptResult))
+  //console.log(ipfsFile.cid)
+
+  const ipfsFileContents = await ipfsCat(ipfsNode, ipfsFile.cid)
+  //console.log(ipfsFileContents.toString())
+
+  const extractResult = ibe.extract(
+    ibeSetupResult.publicParameters, ibeSetupResult.masterSecret, PEER_ID)
+  //console.log(extractResult)
+
+  const decryptResult = ibe.decrypt(
+    ibeSetupResult.publicParameters, extractResult.privateKey, encryptResult.ciphertext);
+  console.log(decryptResult.plaintext)
 }
+
+/* --------------- Helper Functions ----------------------- */
 
 async function addFileToIPFS(node, fileName, content) {
   const fileAdded = await node.add({
@@ -43,4 +58,13 @@ async function addFileToIPFS(node, fileName, content) {
 function readFile(fileName) {
   const data = fs.readFileSync(fileName, {encoding: 'utf8', flag: 'r'})
   return data
+}
+
+async function ipfsCat(ipfsNode, cid) {
+  const chunks = []
+  for await (const chunk of ipfsNode.cat(cid)) {
+      chunks.push(chunk)
+  }
+
+  return chunks
 }
