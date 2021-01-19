7a20d167c9c222aa3047aea0115893f28bd5bd88
paustria <austrp1@unlv.nevada.edu>
2021-01-18 19:33:01 -0800

    retrieve values s form. not done

--- src/browser-client.js
+++ src/browser-client.js
@@ -1,14 +1,32 @@
 'use strict'
 const IPFS = require('ipfs');
+const util = require('util');
 
+// start IPFS instance in the browser
 async function start() {
   document.addEventListener('DOMContentLoaded', async () => {
     const node = await IPFS.create({
       repo: String(Math.random() + Date.now()),
-      init: { alogorithm: 'ed25519' }
+      init: { alogorithm: 'ed25519' },
+      EXPERIMENTAL: { pubsub: true }
     });
     console.log('IPFS node is ready');
+
+    const pid = await node.id();
+    console.log(`Peer ID: ${pid.id}`);
   });
+}
+
+// helper functions
+async function addFileToIPFS(node, fileName, fileContent) {
+  const fileAdded = await node.add({
+    path: fileName,
+    content: fileContent
+  })
+
+  console.log(util.format('Added file to IPFS: %s %s \n',
+    fileAdded.path, fileAdded.cid))
+  return fileAdded
 }
 
 module.exports = {
--- src/index.js
+++ src/index.js
@@ -5,4 +5,17 @@ async function main() {
 }
 
 main();
-console.log('refresh-check');
+console.log('pokemon');
+
+/* ------------ Form Functions -------------- */
+const senderForm = document.forms['s-form'];
+senderForm.addEventListener('submit', e => {
+  e.preventDefault();
+  const senderPid = senderForm.elements['s-pid'].value;
+  const sendToPid = senderForm.elements['s-to-pid'].value;
+  const filePath = senderForm.elements['s-file'].value;
+  const fileName = filePath.split('\\').pop();
+  console.log(senderPid);
+  console.log(sendToPid);
+  console.log(fileName);
+});
