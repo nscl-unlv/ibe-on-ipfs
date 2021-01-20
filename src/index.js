8d4429e8ddf0b150e655304119fa0f1c38470d99
paustria <austrp1@unlv.nevada.edu>
2021-01-20 13:08:18 -0800

    add checkbox to encrypt and ibe setup

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
+const cryptid = require('@cryptid/cryptid-js');
 const IPFS = require('ipfs');
 const util = require('util');
 const path = require('path');
 
+// TODO: move to PKG server
+const ibeSetup = {"publicParameters":{"fieldOrder":"4957260162275438316252191296467403952204205591011298306767298132697199126199675187147343485710518404842036587478511694306658374970862997747937916377323387","subgroupOrder":"1461501637330902918203684832716283019653785059327","pointP":{"x":"1759981139153088184482497969356322559520201589622397133943812668541782200393241524277496056503385648158201477523338181066832749505288665379090507993220725","y":"3890282569570440021375533923621499185864016855804248366316252045554593081403785797928298448639775331710847123166368275020748910562928809965629229968706282"},"pointPpublic":{"x":"3404993143214759727799885561920137966975236015864653478185883898000028882173725660611077186204251149714937904371972374670096155605010504372941794292921490","y":"1150367080250481205505172207325116590334860881325851751687044822363354830943107026772328575858467200059807223776815883664234100946086387601921356739419039"},"securityLevel":0},"success":true}
+
+
 clientMain();
-console.log('pokemon');
 
 async function clientMain() {
-  // start IPFS
   document.addEventListener('DOMContentLoaded', async () => {
+    // start IPFS
     const node = await IPFS.create({
       repo: String(Math.random() + Date.now()),
       init: { alogorithm: 'ed25519' },
@@ -18,6 +22,10 @@ async function clientMain() {
 
     const pid = await node.id();
     console.log(`Peer ID: ${pid.id}`);
+
+    // start IBE
+    console.log('Initializing ID Based Encryption system...\n');
+    const ibe = await cryptid.getInstance();
 
     // get file contents
     let inputFile = document.getElementById('s-file');
@@ -39,7 +47,13 @@ async function clientMain() {
       e.preventDefault();
       const senderPid = senderForm.elements['s-pid'].value;
       const sendToPid = senderForm.elements['s-to-pid'].value;
-      const cid = await addFileIpfs(node, fileName, fileContents);
+      const encryptChecked = senderForm.elements['encrypt'].checked;
+
+      // TODO: encryption checkbox
+
+      // TODO: add encryption function
+
+      //const cid = await addFileIpfs(node, fileName, fileContents);
     });
 
   }); // end addEventListener
