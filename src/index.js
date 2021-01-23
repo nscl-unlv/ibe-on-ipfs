ec91de467a7ba491103c41e607cff1935c793781
Phillipe Austria <austrp1@unlv.nevada.edu>
2021-01-22 16:29:39 -0800

    add buttons to choose client

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
 const { senderMain } = require('./sender');
 
-main();
+let ipfsInstanceExists = false;
 
-async function main() {
-  await senderMain();
-}
+document
+  .getElementById('sender-btn')
+  .addEventListener('click', async () => {
+    if (!ipfsInstanceExists) {
+      ipfsInstanceExists = true;
+      await senderMain();
+    } else {
+      console.log('IPFS instance already running!');
+    }
+  });
+
+document
+  .getElementById('receiver-btn')
+  .addEventListener('click', async () => {
+    if (!ipfsInstanceExists) {
+      ipfsInstanceExists = true;
+      console.log('start receiver node');
+    } else {
+      console.log('IPFS instance already running!');
+    }
+  });
+
